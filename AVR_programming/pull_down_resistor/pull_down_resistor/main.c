0a70752caf5c5da08359503702c950115baef447
abhijith s <abhijith.s7346@gmail.com>
2026-01-31 18:31:00 +0530

    avr pull up and pull down resitor and swithch led

---
+++ AVR_programming/pull_down_resistor/pull_down_resistor/main.c
@@ -0,0 +1,30 @@
+/*
+ * pull_down_resistor.c
+ *
+ * Created: 31-01-2026 18:27:38
+ * Author : DELL
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+
+
+
+int main(void)
+{
+	
+	DDRD &= ~(1<<DDD4);
+	DDRB |= (1<<DDB5);
+	
+	while (1)
+	{
+		
+		if(PIND & (1<<PIND4)){
+			PORTB |= (1<< PORTB5);
+			}else{
+			PORTB &= ~(1<< PORTB5);
+		}
+		
+	}
+}
+
+
